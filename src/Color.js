fbe5888e3ccddec0c8e3f109a9e3852f5b2d8e88
0x0 <21711933@student.uwa.edu.au>
2018-12-17 15:42:48 +0800

    Example of life cycle hooks

--- src/Color.js
+++ src/Color.js
@@ -1,23 +1,63 @@
 import StarRating from './StarRating';
+import PropTypes from 'prop-types';
+import { Component } from 'react';
 
-const Color = ({
-  id,
-  title,
-  color,
-  rating = 0,
-  onRate = f => f,
-  onColorRemove = f => f
-}) => (
-  <section className="color-block">
-    <h2>{title}</h2>
-    <div className="color" style={{ backgroundColor: color }} />
-    <button onClick={() => onColorRemove(id)} className="color-remove">
-      X
-    </button>
-    <div>
-      <StarRating starsSelected={rating} onRate={onRate} />
-    </div>
-  </section>
-);
+class Color extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillMount() {
+    this.style = { backgroundColor: '#ccc' };
+  }
+
+  componentWillUpdate() {
+    this.style = null;
+  }
+
+  shouldComponentUpdate(nextProps) {
+    const { rating } = this.props;
+    return rating !== nextProps.rating; // Prevent re rendering of component if nothing has changed
+  }
+
+  componentDidUpdate(prevProps) {
+    const text = prevProps.rating > this.props.rating ? 'Worse' : 'Better';
+    console.log(`The rating is getting ${text}`);
+  }
+
+  render() {
+    const { onColorRemove, rating, onRate, id, title, color } = this.props;
+
+    return (
+      <section className="color-block" style={this.style}>
+        <h2>{title}</h2>
+        <div className="color" style={{ backgroundColor: color }} />
+        <button onClick={() => onColorRemove(id)} className="color-remove">
+          X
+        </button>
+        <div>
+          <StarRating starsSelected={rating} onRate={onRate} />
+        </div>
+      </section>
+    );
+  }
+}
+
+Color.propTypes = {
+  title: PropTypes.string,
+  id: PropTypes.number,
+  color: PropTypes.string,
+  rating: PropTypes.number,
+  onRate: PropTypes.func,
+  onColorRemove: PropTypes.func
+};
+
+Color.defaultProps = {
+  title: undefined,
+  rating: 0,
+  onRate: f => f,
+  onColorRemove: f => f,
+  color: '#fff'
+};
 
 export default Color;
